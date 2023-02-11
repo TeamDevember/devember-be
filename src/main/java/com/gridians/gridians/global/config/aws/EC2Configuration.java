db917fe5afd924a325dbeb5079edf84f922610c9
dlwodud821@gmail.com <dlwodud821@gmail.com>
2023-02-11 10:45:43 +0900

    chore: Configuration 주석 추가

--- src/main/java/com/gridians/gridians/global/config/aws/EC2Configuration.java
+++ src/main/java/com/gridians/gridians/global/config/aws/EC2Configuration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
+//EC2 사용하지 않을 때 해당 Configuration 사용
 
 @Configuration
 @EnableAutoConfiguration(exclude = ContextInstanceDataAutoConfiguration.class)
