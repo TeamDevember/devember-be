3e23a78a8e6b0bbb8a65ddbd1111e5730ad37fe5
dlwodud821@gmail.com <dlwodud821@gmail.com>
2023-03-01 23:09:22 +0900

    refactor: 코드 일부 수정

--- src/main/java/com/gridians/gridians/global/config/security/filter/MatcherFactory.java
+++ src/main/java/com/gridians/gridians/global/config/security/filter/MatcherFactory.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class MatcherFactory {
 
-    public static String[] permitUrls = {"/user/auth/**", "/cards", "/image/**"};
+    public static String[] permitUrls = {"/user/auth/**", "/cards", "/image/**", "/profile-images/**", "/skill-images/**"};
     public static List<RequestMatcher> getMatcher() {
         List<RequestMatcher> requestMatchers = new ArrayList<>();
         for(String permitUrl : permitUrls) {
